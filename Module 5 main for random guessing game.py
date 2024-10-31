26ef39ce818b1dfaf6981058a1429c91381483e9
Braxton Hartley <brxtnhartley@gmail.com>
2024-10-31 13:24:55 -0500

    Create Module 5 main for random guessing game.py

---
+++ Module 5 main for random guessing game.py
@@ -0,0 +1,18 @@
+from random import randint
+START = 1
+RANGE = 2
+EXIT = 3
+
+
+
+
+def main():
+
+def display_menu():
+
+def players_name():
+    
+def random_number_range():
+    
+def random_number_game():
+    
